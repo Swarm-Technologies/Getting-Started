0e044d6bd4cbbbd7d8f20c7d653100b134b8c2a7
will butler <will.butler@swarm.space>
2021-10-21 14:25:28 -0700

    Update version to 1.2

--- EVAL-KIT/FeatherS2/root/code.py
+++ EVAL-KIT/FeatherS2/root/code.py
@@ -1,7 +1,7 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Copyright (C) 2021, Swarm Technologies, Inc.  All rights reserved.  #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-VERSION = '1.2-rc1'
+VERSION = '1.2'
 import board
 import displayio
 import digitalio
